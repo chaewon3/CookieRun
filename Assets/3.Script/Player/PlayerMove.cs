bccd689fa3b00295241c8706816acec5f140d5d6
chaewon3 <codnjs0619@gmail.com>
2024-08-28 21:43:20 +0900

    stage UI 진행중 끝내야함

--- Assets/3.Script/Player/PlayerMove.cs
+++ Assets/3.Script/Player/PlayerMove.cs
@@ -1,46 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    float moveSpeed = 5f;
+    float moveSpeed = 3f;
     CharacterController cc;
+    Animator anim;
 
     Vector3 dir = Vector3.zero;
 
     private void Awake()
     {
         cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        //print(cc.isGrounded);
         //if (cc.isGrounded)
         //{
-        //    dir.x = Input.GetAxisRaw("Horizontal");
-        //    dir.z = Input.GetAxisRaw("Vertical");
+        //    var h = Input.GetAxisRaw("Horizontal");
+        //    var v = Input.GetAxisRaw("Vertical");
 
+        //    dir = new Vector3(h, 0, v) * moveSpeed * -1;
 
-        //    //var h = Input.GetAxis("Horizontal");
-        //    //var v = Input.GetAxis("Vertical");
-
-        //    print(dir);
-        //    //dir = new Vector3(h, 0, v) * moveSpeed;
         //    if (dir != Vector3.zero)
         //    {
-        //       // transform.rotation = Quaternion.Euler(0, Mathf.Atan2(h, v) * Mathf.Rad2Deg, 0);
+        //        transform.rotation = Quaternion.Euler(0, Mathf.Atan2(h, v) * Mathf.Rad2Deg, 0);
         //        //애니메이션
         //    }
         //    else
         //    { }
         //}
 
-        //dir.y += Physics.gravity.y * Time.deltaTime;
-        //cc.Move(dir *moveSpeed* Time.deltaTime);
+        dir.y += Physics.gravity.y * Time.deltaTime;
+        cc.Move(dir * moveSpeed * Time.deltaTime);
 
-        print($"{0 == Input.GetAxisRaw("Horizontal")}");
 
+
+    }
+
+
+    void OnMove(InputValue value)
+    {
+        Vector3 input = -value.Get<Vector2>();
+        if(cc.isGrounded)
+        {
+            dir = new Vector3(input.x, 0, input.y);
+
+            if (dir != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Euler(0, Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg, 0);
+                anim.SetBool("IsMove", true);
+            }
+            else
+                anim.SetBool("IsMove", false);
+
+    }
     }
 }
