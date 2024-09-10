59be28aa6db71dfa6a6b0ba020898172111b9c5e
chaewon3 <codnjs0619@gmail.com>
2024-09-10 21:38:25 +0900

    움직이는 발판 제작중

--- Assets/3.Script/Stage/MoveBox.cs
+++ Assets/3.Script/Stage/MoveBox.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class MoveBox : MonoBehaviour
 {
-    GameObject MovingBox;
     bool canmove;
+    public List<GameObject> paths;
+    int index = 0;
 
+    float distance;
     private void Update()
     {
         if(canmove)
         {
+            Vector3 Path = paths[index].transform.position;
+            distance = Vector3.Distance(transform.position, Path);
+            
+            //if(distance <= 0)
+
 
         }
     }
