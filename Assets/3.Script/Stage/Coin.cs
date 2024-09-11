48c811392b42784d3dc0f2874b5b666909f7f759
chaewon3 <codnjs0619@gmail.com>
2024-09-11 17:06:04 +0900

    [new] 움직이는 발판 구현

--- Assets/3.Script/Stage/Coin.cs
+++ Assets/3.Script/Stage/Coin.cs
@@ -5,12 +5,44 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public int coin = 100;
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!other.GetComponent<PlayerMove>()) return;
 
         Gamemanager.instance.Coins += coin;
-        Destroy(this.gameObject);
+        StartCoroutine(getcoin(other));
+    }
+
+    IEnumerator getcoin(Collider cookie)
+    {
+        bool get = false;
+        float speed = 3;
+        while(true)
+        {
+            float distance = Vector3.Distance(cookie.transform.position, transform.position);
+
+            Vector3 dir = cookie.transform.position+Vector3.up*0.5f - transform.position;
+            transform.parent.position += dir.normalized * speed * Time.deltaTime;
+
+            if (speed < 6)
+                speed += 0.2f;
+
+            if (distance <= 3f && !get)
+            {
+                get = true;
+                anim.SetTrigger("Get");
+                Destroy(this.gameObject,0.7f);
+            }
+            if (distance <= 0.5f)
+                Destroy(this.gameObject);
+            yield return null;
+        }
     }
 }
--- Assets/3.Script/Stage/MoveBox.cs
+++ Assets/3.Script/Stage/MoveBox.cs
@@ -7,18 +7,33 @@ public class MoveBox : MonoBehaviour
     bool canmove;
     public List<GameObject> paths;
     int index = 0;
+    float movetime = 0.7f;
 
     float distance;
+
+    CharacterController cc;
     private void Update()
     {
         if(canmove)
         {
+            movetime -= Time.deltaTime;
+
             Vector3 Path = paths[index].transform.position;
             distance = Vector3.Distance(transform.position, Path);
-            
-            //if(distance <= 0)
 
-
+            Vector3 dir = Path - transform.position;
+            if(movetime <= 0)
+            {
+                transform.position += dir.normalized * 3 * Time.deltaTime;
+                if (cc != null)
+                    cc.Move(dir.normalized * 3 * Time.deltaTime);
+                if (distance <= 0.1)
+                {
+                    index++;
+                    if (index == paths.Count) index = 0;
+                    movetime = 1.5f;
+                }
+            }        
         }
     }
 
@@ -27,5 +42,14 @@ public class MoveBox : MonoBehaviour
         if (!other.GetComponent<PlayerMove>()) return;
 
         canmove = true;
+
+        cc = other.GetComponent<CharacterController>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.GetComponent<PlayerMove>()) return;
+
+        cc = null;
     }
 }
